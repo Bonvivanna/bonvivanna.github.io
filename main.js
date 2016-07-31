45dcd00b66bdf0afa3b0c8e3358748b15a47ceaf
Bonvivanna <bonvivanna@yopmail.com>
2016-07-31 17:29:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ function initCalendar (month) {
 function displayDays (month) {
   var lastDay = 0;
   var daysContent = '<tr>';
-  var daysOffset = new Date(currentMonth + '1/2016').getDay();
+  var daysOffset = new Date(currentMonth + '/1/2016').getDay();
 
   for (var i = 0; i < daysOffset - 1; i++) {
     daysContent += '<td class="disabled"></td>';
