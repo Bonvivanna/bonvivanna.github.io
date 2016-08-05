6322d659245dd7a026e72477d30c4231a371567c
Bonvivanna <bonvivanna@yopmail.com>
2016-08-05 20:07:17 +0200

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,8 @@
     '31/7': '👳🏾 ❤️ ⚡️',
     '1/8': '🌈 🌮',
     '2/8': '🌈 🍔 ⚡',
-    '3/8': '🌈 ⚡️'
+    '3/8': '🌈 ⚡️',
+    '4/8': '🌈 🏚💤'
   };
 
   var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
